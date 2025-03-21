e327b6916db9569371c622c559f28002c1a5f0db
hemanialaparthi <alaparthi01@allegheny.edu>
2025-03-21 01:33:50 -0400

    feat(Home.jsx): complete the Home page

--- personal-portfolio/src/components/sections/Home.jsx
+++ personal-portfolio/src/components/sections/Home.jsx
@@ -27,10 +27,7 @@ export const Home = () => {
         </h1>
 
         <p className="text-gray-300 text-lg mb-10 leading-relaxed">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-          unknown printer took a galley of type and scrambled it to make a type specimen
-          book.
+        Aspiring software engineer passionate about building innovative solutions. Currently pursuing a double major in Computer Science & Economics at Allegheny College.
         </p>
 
         <div className="flex flex-col sm:flex-row gap-6 mb-10">
@@ -59,24 +56,24 @@ export const Home = () => {
         <div className="flex items-center">
           <p className="text-gray-400 text-base mr-4">Connect with me:</p>
           <div className="flex space-x-6">
-            <a href="#" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
-              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/>
-              </svg>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
-              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z"/>
-              </svg>
-            </a>
-            <a href="#" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
+            <a href="https://github.com/hemanialaparthi" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
               <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
+            <a href="https://www.linkedin.com/in/hemanialaparthi/" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
               <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+              </svg>
+            </a>
+            <a href="https://stackoverflow.com/users/25577383/hemani" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
+              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M17.783 19.823V14.87h1.923v6.899H3.294V14.87h1.923v4.953h12.566zM8.065 14.416l7.521 1.566.405-1.876-7.521-1.566-.405 1.876zm1.383-4.062l6.951 3.285.809-1.74-6.951-3.285-.809 1.74zm2.542-4.307l5.874 4.979 1.246-1.479-5.874-4.979-1.246 1.479zM17.148 0l-1.494 1.127 4.655 6.168 1.494-1.127L17.148 0zM6.823 17.702h7.692v-1.923H6.823v1.923z"/>
+              </svg>
+            </a>
+            <a href="https://leetcode.com/u/hemanialaparthi/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-purple-400 transition-colors transform hover:-translate-y-1 hover:scale-110 duration-300">
+              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M9.92 3.377c.503-.507 1.322-.507 1.825 0l3.394 3.397c.504.506.504 1.328 0 1.833l-1.817 1.822c-.49.492-1.283.496-1.778.008a1.24 1.24 0 01-.004-1.768l.905-.91L9.92 5.202c-.5-.506-.5-1.325 0-1.825zm4.16 8.087c.495-.489 1.288-.484 1.778.008l5.293 5.3c.506.507.506 1.328 0 1.835l-5.293 5.298c-.495.492-1.283.488-1.778-.008a1.24 1.24 0 01-.004-1.766l.905-.91-1.824-1.826c-.504-.504-.504-1.326 0-1.832l1.824-1.826-.905-.91a1.24 1.24 0 01.004-1.766zm-5.95 8.72c-.504-.506-.504-1.328 0-1.834l1.824-1.826-.905-.91a1.24 1.24 0 01.004-1.766c.495-.489 1.288-.484 1.778.008l1.824 1.826c.504.506.504 1.328 0 1.832l-1.824 1.826.905.91c.5.506.5 1.325 0 1.825-.495.496-1.283.5-1.778.008l-3.393-3.397z"/>
               </svg>
             </a>
           </div>
@@ -93,7 +90,7 @@ export const Home = () => {
           {/* Profile image with subtle shadow */}
           <div className="relative rounded-full bg-gradient-to-b from-gray-900 to-black p-3 shadow-2xl">
             <img
-              src="/api/placeholder/400/400"
+              src="../../../public/hemani1.png"
               alt="Hemani Alaparthi"
               className="w-64 h-64 md:w-96 md:h-96 rounded-full object-cover"
             />
