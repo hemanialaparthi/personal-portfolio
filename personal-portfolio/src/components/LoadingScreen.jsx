127e7f0e173ec52d4e7f4b42939efaab516133af
hemanialaparthi <alaparthi01@allegheny.edu>
2025-04-02 17:57:47 -0400

    fix(LoadingScreen.jsx): fix loading screen

--- personal-portfolio/src/components/LoadingScreen.jsx
+++ personal-portfolio/src/components/LoadingScreen.jsx
@@ -28,11 +28,11 @@ export const LoadingScreen = ({onComplete}) => {
 
     return (
         <div className="fixed inset-0 z-50 bg-auto text-purple-400 flex flex-col items-center justify-center">
-            <div className="mb-4 text-4xl font-mono font-bold">
+            <div className="mb-4 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-mono font-bold text-center px-4">
                 {text}<span className="animate-blink ml-1"> | </span>
             </div>
 
-            <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+            <div className="w-[150px] sm:w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
                 <div className="w-[40%] h-full bg-purple-400 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
             </div>
         </div>
